1cf663937bc6c7bd245cb84cdf99bcd8f3831379
Chris Elrod <elrodc@gmail.com>
2019-10-05 16:33:02 -0500

    Finished ReverseExprDiff refactoring.

--- src/ReverseDiffExpressionsBase.jl
+++ src/ReverseDiffExpressionsBase.jl
@@ -1,6 +1,8 @@
 module ReverseDiffExpressionsBase
 
-using StackPointers, SIMDPirates
+using StackPointers, SIMDPirates, VectorizationBase
+
+import SIMDPirates: vadd, vsum, vifelse
 
 export Zero, One
 
@@ -8,25 +10,29 @@ export Zero, One
 Additive identity.
 """
 struct Zero end
-@inline Base.+(a::Zero, b) = b
-@inline Base.+(a, b::Zero) = a
-@inline Base.+(a::Zero, b::Zero) = Zero()
-@inline Base.+(sptr::StackPointer, a::Zero, b) = (sptr, b)
-@inline Base.+(sptr::StackPointer, a, b::Zero) = (sptr, a)
-@inline Base.+(sptr::StackPointer, a::Zero, b::Zero) = (sptr, Zero())
+@inline Base.:+(a::Zero, b) = b
+@inline Base.:+(a, b::Zero) = a
+@inline Base.:+(a::Zero, b::Zero) = Zero()
+@inline Base.:+(sptr::StackPointer, a::Zero, b) = (sptr, b)
+@inline Base.:+(sptr::StackPointer, a, b::Zero) = (sptr, a)
+@inline Base.:+(sptr::StackPointer, a::Zero, b::Zero) = (sptr, Zero())
 """
 Multiplicative identity.
 """
 struct One end
-@inline Base.*(a::One, b) = b
-@inline Base.*(a, b::One) = a
-@inline Base.*(a::One, b::One) = One()
-@inline Base.*(sptr::StackPointer, a::One, b) = (sptr, b)
-@inline Base.*(sptr::StackPointer, a, b::One) = (sptr, a)
-@inline Base.*(sptr::StackPointer, a::One, b::One) = (sptr, One())
+@inline Base.:*(a::One, b) = b
+@inline Base.:*(a, b::One) = a
+@inline Base.:*(a::One, b::One) = One()
+@inline Base.:*(sptr::StackPointer, a::One, b) = (sptr, b)
+@inline Base.:*(sptr::StackPointer, a, b::One) = (sptr, a)
+@inline Base.:*(sptr::StackPointer, a::One, b::One) = (sptr, One())
 
 Base.size(::Zero) = Core.tuple()
 Base.size(::One) = Core.tuple()
+
+function ∂getindex end
+function ∂mul end
+
 
 include("target.jl")
 include("seed_increments.jl")
