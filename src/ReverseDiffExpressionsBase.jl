d3037bec9f7aea511399814bf45b6c2b67287498
Chris Elrod <elrodc@gmail.com>
2019-10-28 00:04:22 -0400

    In the middle of a major rewrite; everything broken.

--- src/ReverseDiffExpressionsBase.jl
+++ src/ReverseDiffExpressionsBase.jl
@@ -4,43 +4,76 @@ using StackPointers, SIMDPirates, VectorizationBase
 
 import SIMDPirates: vadd, vsum, vifelse
 
-export Zero, One
+export uninitialized, isinitialized
 
-"""
-Additive identity.
-"""
-struct Zero end
-@inline Base.:+(a::Zero, b) = b
-@inline Base.:+(a, b::Zero) = a
-@inline Base.:+(a::Zero, b::Zero) = Zero()
-@inline Base.:+(sptr::StackPointer, a::Zero, b) = (sptr, b)
-@inline Base.:+(sptr::StackPointer, a, b::Zero) = (sptr, a)
-@inline Base.:+(sptr::StackPointer, a::Zero, b::Zero) = (sptr, Zero())
-"""
-Multiplicative identity.
-"""
-struct One end
-@inline Base.:*(a::One, b) = b
-@inline Base.:*(a, b::One) = a
-@inline Base.:*(a::One, b::One) = One()
-@inline Base.:*(sptr::StackPointer, a::One, b) = (sptr, b)
-@inline Base.:*(sptr::StackPointer, a, b::One) = (sptr, a)
-@inline Base.:*(sptr::StackPointer, a::One, b::One) = (sptr, One())
+# export Zero, One
 
-Base.size(::Zero) = Core.tuple()
-Base.size(::One) = Core.tuple()
+# """
+# Additive identity.
+# """
+# struct Zero end
+# @inline Base.:+(a::Zero, b) = b
+# @inline Base.:+(a, b::Zero) = a
+# @inline Base.:+(a::Zero, b::Zero) = Zero()
+# @inline Base.:+(sptr::StackPointer, a::Zero, b) = (sptr, b)
+# @inline Base.:+(sptr::StackPointer, a, b::Zero) = (sptr, a)
+# @inline Base.:+(sptr::StackPointer, a::Zero, b::Zero) = (sptr, Zero())
+# """
+# Multiplicative identity.
+# """
+# struct One end
+# @inline Base.:*(a::One, b) = b
+# @inline Base.:*(a, b::One) = a
+# @inline Base.:*(a::One, b::One) = One()
+# @inline Base.:*(sptr::StackPointer, a::One, b) = (sptr, b)
+# @inline Base.:*(sptr::StackPointer, a, b::One) = (sptr, a)
+# @inline Base.:*(sptr::StackPointer, a::One, b::One) = (sptr, One())
+
+# Base.size(::Zero) = Core.tuple()
+# Base.size(::One) = Core.tuple()
+
+abstract type AbstractUninitializedReference{T} end
+"""
+This is a struct wrapping a RefValue rather than a mutable struct
+because the ReverseDiff API wants the initialized parameters to be
+Refs of some kind; it is simpler to wrap the first access in an
+Uninitialized struct than a mutable struct.
+"""
+struct UninitializedRef{T} <: AbstractUninitializedReference{T}
+    data::Base.RefValue{T}
+end
+@inline Base.getindex(uninit::UninitializedRef) = uninit.data[]
+@inline Base.setindex!(uninit::UninitializedRef{T}, v::T) where {T} = uninit.data[] = v
+struct UninitializedPtr{T} <: AbstractUninitializedReference{T}
+    ptr::Ptr{T}
+end
+@inline Base.getindex(uninit::UninitializedPtr) = VectorizationBase.load(uninit.ptr)
+@inline Base.setindex!(uninit::UninitializedPtr{T}, v::T) where {T} = VectorizationBase.store!(uninit.ptr, v)
 
 function ∂getindex end
 function ∂mul end
+# function radj end
+@inline seed(x) = Ref(x)
+@inline uninitialized(::Nothing) = nothing
+@inline uninitialized(x::Base.RefValue) = Uninitialized(x)
+@inline uninitialized(x::VectorizationBase.Pointer) = UninitializedPtr(pointer(x))
+isinitialized(::Any) = true
+isinitialized(::Type{<:UninitializedRef}) = false
+isinitialized(::Type{<:UninitializedPtr}) = false
+
+# Base.Broadcast.materialize to be used for instantiating lazy objects.
+# @inline grad
 
 
 include("target.jl")
 include("seed_increments.jl")
 
+adj(out, a) = Symbol("##∂", out, "/∂", a, "##")
+adj(a) = adj(:target, a)
 
-@def_stackpointer_fallback RESERVED_INCREMENT_SEED_RESERVED RESERVED_DECREMENT_SEED_RESERVED RESERVED_NMULTIPLY_SEED_RESERVED RESERVED_MULTIPLY_SEED_RESERVED ∂getindex
+@def_stackpointer_fallback RESERVED_INCREMENT_SEED_RESERVED! ∂getindex seed radj uninitialized
 function __init__()
-    @add_stackpointer_method RESERVED_INCREMENT_SEED_RESERVED RESERVED_DECREMENT_SEED_RESERVED RESERVED_NMULTIPLY_SEED_RESERVED RESERVED_MULTIPLY_SEED_RESERVED ∂getindex
+    @add_stackpointer_method RESERVED_INCREMENT_SEED_RESERVED! ∂getindex seed radj uninitialized
 end
 
 
--- src/seed_increments.jl
+++ src/seed_increments.jl
@@ -1,63 +1,26 @@
 
 
-# These functions are all registered as supporting StackPointers,
-# as we will only use them alongside StackPointers, we only need
-# to write versions taking them as the first argument.
-#
-# Additionally, RESERVED_INCREMENT_SEED_RESERVED and
-# RESERVED_DECREMENT_SEED_RESERVED (is this used?)
-# are allowed to mutate the last (the incremented) argument.
-# 
 
-@inline RESERVED_INCREMENT_SEED_RESERVED(sp::StackPointer, a) = (sp,a)
-@inline RESERVED_DECREMENT_SEED_RESERVED(sp::StackPointer, a) = (sp,-a)
-@inline function RESERVED_INCREMENT_SEED_RESERVED(sp::StackPointer, a, b, c)
-    sp, SIMDPirates.vmuladd(a, b, c)
-end
-@inline function RESERVED_DECREMENT_SEED_RESERVED(sp::StackPointer, a, b, c)
-    sp, SIMDPirates.fnmadd(a, b, c)
-end
-@inline function RESERVED_INCREMENT_SEED_RESERVED(sp::StackPointer, a, b)
-    +(sp, a, b)
-end
-@inline function RESERVED_DECREMENT_SEED_RESERVED(sp::StackPointer, a, b)
-    -(sp, b, a)
-end
 
-@inline RESERVED_MULTIPLY_SEED_RESERVED(sp::StackPointer, a) = sp, a
-@inline function RESERVED_NMULTIPLY_SEED_RESERVED(sp::StackPointer, a)
-    sp, -a
+@inline RESERVED_INCREMENT_SEED_RESERVED!(c::AbstractUninitializedReference, a) = (c[] =  a; nothing)
+@inline function RESERVED_INCREMENT_SEED_RESERVED!(c, a)
+    c[] = SIMDPirates.vadd(c[], a); nothing
 end
-
-@inline function RESERVED_MULTIPLY_SEED_RESERVED(sp::StackPointer, a, b)
-    *(sp, a, b)
+@inline function RESERVED_INCREMENT_SEED_RESERVED!(c::AbstractUninitializedReference, a, b)
+    c[] = SIMDPirates.vmul(a, b); nothing
 end
-@inline function RESERVED_NMULTIPLY_SEED_RESERVED(sp::StackPointer, a, b)
-    *(sp, -a, b)
+@inline function RESERVED_INCREMENT_SEED_RESERVED!(c, a, b)
+    c[] = SIMDPirates.vmuladd(a, b, c[]); nothing
 end
-
-@inline function RESERVED_INCREMENT_SEED_RESERVED(sp::StackPointer, ::One, b, c)
-    RESERVED_INCREMENT_SEED_RESERVED(sp, b, c)
-end
-@inline function RESERVED_DECREMENT_SEED_RESERVED(sp::StackPointer, ::One, b, c)
-    RESERVED_DECREMENT_SEED_RESERVED(sp, b, c)
-end
-
-@inline RESERVED_MULTIPLY_SEED_RESERVED(sp::StackPointer, ::One, b) = (sp, b)
-@inline RESERVED_NMULTIPLY_SEED_RESERVED(sp::StackPointer, ::One, b) = (sp, -b)
-
-@inline function RESERVED_INCREMENT_SEED_RESERVED(sp::StackPointer, a, ::One, c)
-    RESERVED_INCREMENT_SEED_RESERVED(sp, a, c)
-end
-@inline function RESERVED_DECREMENT_SEED_RESERVED(sp::StackPointer, a, ::One, c)
-    RESERVED_DECREMENT_SEED_RESERVED(sp, a, c)
-end
-
-@inline RESERVED_INCREMENT_SEED_RESERVED(sp::StackPointer, ::One, b) = (sp, b)
-
-@inline RESERVED_MULTIPLY_SEED_RESERVED(sp::StackPointer, ::One) = (sp, One())
-@inline RESERVED_MULTIPLY_SEED_RESERVED(sp::StackPointer, a, ::One) = (sp,a)
-@inline RESERVED_NMULTIPLY_SEED_RESERVED(sp::StackPointer, a, ::One) = (sp,-a)
-@inline RESERVED_MULTIPLY_SEED_RESERVED(sp::StackPointer, ::One, ::One) = (sp, One())
+# @inline RESERVED_DECREMENT_SEED_RESERVED!(c::Uninitialized, a) = (c[] = -a; nothing)
+# @inline function RESERVED_DECREMENT_SEED_RESERVED!(c, a)
+    # c[] = SIMDPirates.vsub(c[], a); nothing
+# end
+# @inline function RESERVED_DECREMENT_SEED_RESERVED!(c::Uninitialized, a, b)
+    # c[] = - SIMDPirates.vmul(a, b); nothing
+# end
+# @inline function RESERVED_DECREMENT_SEED_RESERVED!(c, a, b)
+    # c[] = SIMDPirates.fnmadd(a, b, c[]); nothing
+# end
 
 
