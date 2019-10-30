d55fc46b0d8425f80b7eeb998a8f5fa5452a56c5
chriselrod <elrodc@gmail.com>
2019-10-30 16:48:27 -0400

    Added Zero and One, which were previously removed.

--- src/ReverseDiffExpressionsBase.jl
+++ src/ReverseDiffExpressionsBase.jl
@@ -6,31 +6,62 @@ import SIMDPirates: vadd, vsum, vifelse
 
 export uninitialized, isinitialized
 
-# export Zero, One
+export Zero, One
 
-# """
-# Additive identity.
-# """
-# struct Zero end
-# @inline Base.:+(a::Zero, b) = b
-# @inline Base.:+(a, b::Zero) = a
-# @inline Base.:+(a::Zero, b::Zero) = Zero()
-# @inline Base.:+(sptr::StackPointer, a::Zero, b) = (sptr, b)
-# @inline Base.:+(sptr::StackPointer, a, b::Zero) = (sptr, a)
-# @inline Base.:+(sptr::StackPointer, a::Zero, b::Zero) = (sptr, Zero())
-# """
-# Multiplicative identity.
-# """
-# struct One end
-# @inline Base.:*(a::One, b) = b
-# @inline Base.:*(a, b::One) = a
-# @inline Base.:*(a::One, b::One) = One()
-# @inline Base.:*(sptr::StackPointer, a::One, b) = (sptr, b)
-# @inline Base.:*(sptr::StackPointer, a, b::One) = (sptr, a)
-# @inline Base.:*(sptr::StackPointer, a::One, b::One) = (sptr, One())
+"""
+ Additive identity.
+"""
+struct Zero end
+@inline Base.:+(::Zero, a) = a
+@inline Base.:+(a, ::Zero) = a
+@inline Base.:+(::Zero, ::Zero) = Zero()
+@inline Base.:+(sptr::StackPointer, ::Zero, a) = (sptr, a)
+@inline Base.:+(sptr::StackPointer, a, ::Zero) = (sptr, a)
+@inline Base.:+(sptr::StackPointer, ::Zero, ::Zero) = (sptr, Zero())
+@inline SIMDPirates.vadd(::Zero, a) = a
+@inline SIMDPirates.vadd(a, ::Zero) = a
+@inline SIMDPirates.vadd(::Zero, ::Zero) = Zero()
+@inline SIMDPirates.vadd(sptr::StackPointer, ::Zero, a) = (sptr, a)
+@inline SIMDPirates.vadd(sptr::StackPointer, a, ::Zero) = (sptr, a)
+@inline SIMDPirates.vadd(sptr::StackPointer, ::Zero, ::Zero) = (sptr, Zero())
 
-# Base.size(::Zero) = Core.tuple()
-# Base.size(::One) = Core.tuple()
+@inline Base.:*(::Zero, a) = Zero()
+@inline Base.:*(a, ::Zero) = Zero()
+@inline Base.:*(::Zero, ::Zero) = Zero()
+@inline Base.:*(sptr::StackPointer, ::Zero, a) = (sptr, Zero())
+@inline Base.:*(sptr::StackPointer, a, ::Zero) = (sptr, Zero())
+@inline Base.:*(sptr::StackPointer, ::Zero, ::Zero) = (sptr, Zero())
+@inline SIMDPirates.vmul(::Zero, a) = Zero()
+@inline SIMDPirates.vmul(a, ::Zero) = Zero()
+@inline SIMDPirates.vmul(::Zero, ::Zero) = Zero()
+@inline SIMDPirates.vmul(sptr::StackPointer, ::Zero, a) = (sptr, Zero())
+@inline SIMDPirates.vmul(sptr::StackPointer, a, ::Zero) = (sptr, Zero())
+@inline SIMDPirates.vmul(sptr::StackPointer, ::Zero, ::Zero) = (sptr, Zero())
+
+
+"""
+Multiplicative identity.
+"""
+struct One end
+@inline Base.:*(::One, a) = a
+@inline Base.:*(a, ::One) = a
+@inline Base.:*(::One, ::One) = One()
+@inline Base.:*(sptr::StackPointer, ::One, a) = (sptr, a)
+@inline Base.:*(sptr::StackPointer, a, ::One) = (sptr, a)
+@inline Base.:*(sptr::StackPointer, ::One, ::One) = (sptr, One())
+@inline SIMDPirates.vmul(::One, a) = a
+@inline SIMDPirates.vmul(a, ::One) = a
+@inline SIMDPirates.vmul(::One, ::One) = One()
+@inline SIMDPirates.vmul(sptr::StackPointer, ::One, a) = (sptr, a)
+@inline SIMDPirates.vmul(sptr::StackPointer, a, ::One) = (sptr, a)
+@inline SIMDPirates.vmul(sptr::StackPointer, ::One, ::One) = (sptr, One())
+@inline Base.inv(::One) = One()
+@inline Base.FastMath.inv_fast(::One) = One()
+@inline Base.:/(a, ::One) = a
+@inline Base.FastMath.div_fast(a, ::One) = a
+
+Base.size(::Zero) = Core.tuple()
+Base.size(::One) = Core.tuple()
 
 abstract type AbstractUninitializedReference{T} end
 """
